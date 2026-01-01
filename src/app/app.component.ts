921ce5638ee26c97d3cfb4937af5c9b99c3009a3
pety02 <pety02@abv.bg>
2026-01-01 15:51:08 +0200

    Registration form styled.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoginFormComponent} from './login-form/login-form.component';
-import {NgIf} from '@angular/common';
+import {NgClass, NgIf} from '@angular/common';
 import {RegistrationFormComponent} from './registration-form/registration-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginFormComponent, RegistrationFormComponent, NgIf, RegistrationFormComponent, RegistrationFormComponent, RegistrationFormComponent, RegistrationFormComponent],
+  imports: [RouterOutlet, LoginFormComponent, RegistrationFormComponent, NgIf, RegistrationFormComponent, RegistrationFormComponent, RegistrationFormComponent, RegistrationFormComponent, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
